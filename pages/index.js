32cdd4f3c8b8140d4a28daf662bfb825400a697e
Cole <coletaylor017@gmail.com>
2020-04-30 02:02:35 -0600

    inital commit

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class Index extends Component {
+    static async getInitialProps() {
+        let artPieces;
+        try {
+            artPieces = await axios.get(process.env.CRUD_API_URI + "/pieces");
+        }
+        catch (error) {
+            console.log("error fetching /pieces");
+        }
+        return {
+            pieces: artPieces.data
+        }
+    }
+    render() {
+        return <div>
+            <h1>The index page!</h1>
+            {this.props.pieces.map(piece => piece.title)}
+        </div>
+    }
+}
